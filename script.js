4246ee01183fd778aadfbedc3c519b6350b1e712
BSorez <bsorez@gmail.com>
2026-03-14 18:08:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 // 1. CONFIGURATION
-const surveyConfig = [
-    { id: "email", question: "Votre email", type: "email", required: true },
-    { id: "usage", question: "Utilisez-vous notre service ?", type: "select", options: ["Oui", "Non", "Parfois"], required: true },
-    { id: "note", question: "Note sur 10", type: "range", min: 1, max: 10, required: false },
-    { id: "comm", question: "Commentaires", type: "text", required: false }
-];
 
 let currentStep = 0;
 const container = document.getElementById('survey-container');
